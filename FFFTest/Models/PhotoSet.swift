b5336d3bcabe22e1ff592599d7949d2c510551c8
Nuno Gonçalves <numicago@gmail.com>
2017-01-26 00:03:34 +0000

    Introducing PhotoSet

---
+++ FFFTest/Models/PhotoSet.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoSet.swift
+//  FFFTest
+//
+//  Created by Nuno Gonçalves on 25/01/17.
+//  Copyright © 2017 Nuno Gonçalves. All rights reserved.
+//
+
+import Foundation
+
+struct PhotoSet {
+    
+    let currentPage: Int
+    let totalPages: Int
+    let photos: [FlickrPhoto]
+    
+    var hasMorePages: Bool {
+        return currentPage == totalPages
+    }
+    
+    var isFirstPage: Bool {
+        return currentPage == 1
+    }
+    
+    static let empty = PhotoSet(currentPage: 0, totalPages: 0, photos: [])
+}

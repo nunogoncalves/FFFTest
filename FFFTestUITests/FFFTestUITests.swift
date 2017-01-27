1b9c6e2b31ba3e8953874b38f7f9feafc478a428
Nuno Gonçalves <numicago@gmail.com>
2017-01-27 23:02:05 +0000

    ui ... tests? ¯\_(ツ)_/¯

--- FFFTestUITests/FFFTestUITests.swift
+++ FFFTestUITests/FFFTestUITests.swift
@@ -12,25 +12,43 @@ class FFFTestUITests: XCTestCase {
         
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
+    }
+    
+    func testExample() {
+        let app = XCUIApplication()
+        XCUIDevice.shared().orientation = .portrait
+        
+        loadFirstScreenAndTapARandomCell(on: app)
+        app.otherElements
+            .containing(.navigationBar, identifier:"FFFTest.ImageDetailView")
+            .children(matching: .other)
+            .element
+            .children(matching: .other)
+            .element
+            .children(matching: .other)
+            .element
+            .children(matching: .image)
+            .element
+            .tap()
+    }
+    
+    private func loadFirstScreenAndTapARandomCell(on app: XCUIApplication) {
+        let someRandomCell = app.collectionViews.children(matching: .cell).element(boundBy: 7)
+        
+        let existsPredicate = NSPredicate(format: "exists == true")
+        expectation(for: existsPredicate, evaluatedWith: someRandomCell, handler: nil)
+        
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        XCTAssert(someRandomCell.exists)
+        someRandomCell
+            .otherElements
+            .children(matching: .image)
+            .element
+            .tap()
+        
+    }
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-//    func testExample() {
-//        // Use recording to get started writing UI tests.
-//        // Use XCTAssert and related functions to verify your tests produce the correct results.
-//    }
-    
 }
